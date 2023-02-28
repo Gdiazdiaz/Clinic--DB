72b31349172b4352ee6943a4d9fad8ea91050058
sambeck87 <sandro_mipb@hotmail.com>
2023-02-28 10:50:40 -0600

    Add invoices, invoice_items, and med_his_treat tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -16,3 +16,25 @@ CREATE TABLE medical_histories(
    patient_id integer References patients (id),
 	status text
 );
+
+CREATE TABLE invoices(
+   id serial Primary key,
+   total_amount decimal,
+   generated_at timestamp,
+   payed_at timestamp,
+   medical_history_id integer References medical_histories (id)
+);
+
+CREATE TABLE invoice_items(
+   id serial Primary key,
+   unit_price decimal,
+   quantity integer,
+   total_price decimal,
+   invoice_id integer References invoices (id),
+   treatment_id integer References treatments (id)
+);
+
+Create table med_hist_treat(
+   medical_history_id integer References medical_histories (id),
+   treatment_id integer References treatments (id)
+);
